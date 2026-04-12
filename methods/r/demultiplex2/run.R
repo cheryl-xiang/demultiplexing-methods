486dff9f0db0e80d1a76cdf5b779a7cb5da22acd
cheryl-xiang <cherylxiang12@gmail.com>
2026-04-12 16:50:42 -0700

    fix demultiplex2 script

--- methods/r/demultiplex2/run.R
+++ methods/r/demultiplex2/run.R
@@ -1,5 +1,8 @@
 #script to run demultiplex2 on dataset_1 (GSM4904942_8donor_PBMC_AH_MULTI_matrix.csv)
 
+#to run in terminal: conda activate demux-r ---> Rscript methods/r/demultiplex2/run.R
+
+
 library(deMULTIplex2)
 library(tidyverse)
 
@@ -12,38 +15,38 @@ res <- demultiplexTags(data)
 
 #get classifications
 classifications <- res$assign_table %>%
-  rownames_to_column("cell_barcode") %>%
+  rownames_to_column('cell_barcode') %>%
   select(cell_barcode, classification = final_assign)
 
 #save classifications
-dir.create("results/demultiplex2/dataset_1", recursive = TRUE, showWarnings = FALSE)
+dir.create('results/demultiplex2/dataset_1', recursive = TRUE, showWarnings = FALSE)
 write.csv(classifications,
-          "results/demultiplex2/dataset_1/classifications.csv",
+          'results/demultiplex2/dataset_1/classifications.csv',
           row.names = FALSE)
 
 #save summary counts
 summary_counts <- classifications %>%
   mutate(classification = case_when(
-    classification == "negative" ~ "negative",
-    str_detect(classification, "multiplet") ~ "multiplet",
-    TRUE ~ "singlet"
+    classification == 'negative' ~ 'negative',
+    str_detect(classification, 'multiplet') ~ 'multiplet',
+    TRUE ~ 'singlet'
   )) %>%
   count(classification) %>%
-  mutate(dataset = "dataset_1", method = "demultiplex2")
+  mutate(dataset = 'dataset_1', method = 'demultiplex2')
 
 write.csv(summary_counts,
-          "results/demultiplex2/dataset_1/summary.csv",
+          'results/demultiplex2/dataset_1/summary.csv',
           row.names = FALSE)
 
 # move assignment pdf to results folder
 pdf_file <- list.files(pattern = ".*assignment\\.pdf$")
 if (length(pdf_file) > 0) {
-  file.rename(pdf_file, paste0("results/demultiplex2/dataset_1/", pdf_file))
+  file.rename(pdf_file, paste0('results/demultiplex2/dataset_1/', pdf_file))
 }
 
 # move Rplots.pdf to results folder
-if (file.exists("Rplots.pdf")) {
-  file.rename("Rplots.pdf", "results/demultiplex2/dataset_1/Rplots.pdf")
+if (file.exists('Rplots.pdf')) {
+  file.rename('Rplots.pdf', 'results/demultiplex2/dataset_1/Rplots.pdf')
 }
 
 #print classification counts
