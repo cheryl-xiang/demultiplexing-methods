2c370b5db76ee1c16b224a4d85049859c152bc55
cheryl-xiang <cherylxiang12@gmail.com>
2026-04-30 12:10:32 -0700

    update yml

--- methods/r/bffraw/run.R
+++ methods/r/bffraw/run.R
@@ -8,3 +8,10 @@
 args <- commandArgs(trailingOnly = TRUE)
 dataset_id <- args[1]
 input_file <- args[2]
+
+library(cellhashR)
+library(tidyverse)
+
+#data loading
+data <- read.csv(input_file, row.names = 1)
+data <- data[, !colnames(data) %in% c('nUMI', 'nUMI_total')] #will need to check other datasets for diff col names !!
