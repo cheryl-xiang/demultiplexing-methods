f8296cb3483be7fb7e409852a927b2eca99824e4
cheryl-xiang <cherylxiang12@gmail.com>
2026-06-03 23:11:54 -0700

    rerun htodemux

--- methods/r/htodemux/run.R
+++ methods/r/htodemux/run.R
@@ -2,24 +2,34 @@
 
 #to run in terminal: 
 #    (1) conda activate demux-r 
-#    (2) Rscript methods/r/htodemux/run.R dataset_# data/dataset_#/hto/file_name.csv
+#    (2) Rscript methods/r/htodemux/run.R dataset data/dataset/hto/file_name.csv n_barcodes
 
 #read command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 dataset_id <- args[1]
 input_file <- args[2]
+if (length(args) >= 3) {
+  n_barcodes <- as.integer(args[3])
+} else {
+  n_barcodes <- NULL
+}
 
 library(Seurat)
 library(tidyverse) 
 
 #data loading
 data <- read.csv(input_file, row.names = 1)
-data <- data[, !colnames(data) %in% c('nUMI', 'nUMI_total')]  #will need to check other datasets for diff col names !!
+data <- data[, !colnames(data) %in% c('nUMI', 'nUMI_total')]
 
-mat <- t(data)     #expects barcodes as rows, cells as cols
-mat <- mat[rowSums(mat) > 0, ]
-mat <- mat[, colSums(mat) > 10]
+if (!is.null(n_barcodes)) {
+  data <- data[, 1:n_barcodes]
+  print(paste('Using first', n_barcodes, 'barcodes'))
+} else {
+  print(paste('Using all', ncol(data), 'barcodes'))
+}
 
+#transpose
+mat <- t(as.matrix(data))
 mat_sparse <- Matrix::Matrix(mat, sparse = TRUE)
 
 seurat_obj <- CreateSeuratObject(counts = mat_sparse)
@@ -32,6 +42,8 @@ seurat_obj <- NormalizeData(seurat_obj, assay = 'HTO', normalization.method = "C
 
 #run HTOdemux
 res <-  HTODemux(seurat_obj, assay = 'HTO', positive.quantile = 0.99)
+#res <-  HTODemux(seurat_obj, assay = 'HTO', positive.quantile = 0.99, kfunc = 'kmeans')  #<-- Runs on Winkler, but not default
+
 
 #get classifications
 classifications <- data.frame(
